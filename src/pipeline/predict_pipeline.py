b9797d615f036f324ec2b5bbfb299769990e332a
kashyap1429 <kashyappatel9108@gmail.com>
2023-10-06 15:23:30 +0530

    Precidting

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -11,8 +11,8 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            model_path="artifacts\model.pkl"
-            preprocessor_path="artifacts\preprocessor.pkl"
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
             print("Before Loading")
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
@@ -23,8 +23,6 @@ class PredictPipeline:
         
         except Exception as e:
             raise CustomException(e,sys)
-
-
 
 class CustomData:
     def __init__(  self,
@@ -37,17 +35,11 @@ class CustomData:
         writing_score: int):
 
         self.gender = gender
-
         self.race_ethnicity = race_ethnicity
-
         self.parental_level_of_education = parental_level_of_education
-
         self.lunch = lunch
-
         self.test_preparation_course = test_preparation_course
-
         self.reading_score = reading_score
-
         self.writing_score = writing_score
 
     def get_data_as_data_frame(self):
